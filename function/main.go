d51bbd18c7a219ed61a2dc9a56ba112c2a9ce5b6
nghiatn <nghiatn@vng.com.vn>
2022-01-06 23:36:07 +0700

    + add stuff for function

--- function/main.go
+++ function/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 // https://stackoverflow.com/questions/8263546/whats-the-difference-of-functions-and-methods-in-go
+// functions in go can be treated as types as well, they can be passed around like variables
+// slice and map are always passed by value of pointers
 
 type Person struct {
 	firstName string
@@ -29,10 +31,30 @@ func main() {
 
 	result := partition("aab")
 	fmt.Println("result", result)
+
+	// anonymous function
+	// functions don't have names if they are:
+	// - immediately invoked
+	// - assigned to a variable or passed as an argument to a function
+	var myFunc func() = func() {
+		fmt.Println("myFunc")
+	}
+	myFunc()
+
+	// methods:
+	// function that executes in context of a type
+	// func(g greeter) greet() // g is called a receiver
 }
 
-/*
-* (also know as indirection operator) is used to “dereference” pointer variables,
+type counter string
+
+func divide(a, b int) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("cannot provide by zero")
+	}
+	return float64(a / b), nil
+} /*
+* (also known as indirection operator) is used to “dereference” pointer variables,
 dereferencing a pointer gives us access to the value the pointer points to.
 In this case: *myString = "anotherString"
 basically you are saying to the compiler:
@@ -42,9 +64,26 @@ func myFunction(myString *string) {
 	*myString = "anotherString"
 }
 
+/* Variadic parameter: Go runtime takes the last arguments which are passed in, and wrap them up into a slice which
+has a name of the variable that we have here
+*/
+func mSum(values ...int) int {
+	sum := 0
+	for _, value := range values {
+		sum += value
+	}
+
+	return sum
+}
+
+/*
+This is just a random leetcode problem solved in go
+*/
 func partition(s string) [][]string {
 	result := new([][]string)
 	helper(result, s, *new([]string), 0)
+	// can return address of local variable
+	// automatically promoted from local memory (stack) to shared memory (heap)
 	return *result
 }
 
@@ -73,5 +112,24 @@ func isPalindrome(s string, start int, end int) bool {
 		start++
 		end--
 	}
+	return true
+}
+
+func carPooling(trips [][]int, capacity int) bool {
+	stops := make([]int, 1001)
+
+	for _, trip := range trips {
+		stops[trip[1]] += trip[0]
+		stops[trip[2]] -= trip[0]
+	}
+
+	cap := 0
+	for i := 0; i < 1001; i++ {
+		cap += stops[i]
+		if cap > capacity {
+			return false
+		}
+	}
+
 	return true
 }
