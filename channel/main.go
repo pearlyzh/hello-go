d0bd22ca99344d9b5cac019144186552b66ba002
nghiatn <nghiatn@vng.com.vn>
2022-01-11 17:07:35 +0700

    + add channel

---
+++ channel/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Person struct {
+	name string
+	age  int
+}
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	var channel = make(chan Person)
+	wg.Add(2)
+	go consume(channel)
+	go produce(channel)
+	wg.Wait()
+}
+
+func consume(channel chan Person) {
+	start := time.Now()
+	data := <-channel
+	elapsed := time.Since(start)
+	fmt.Printf("Hey, I have consumed this %v after waiting time %v\n", data, elapsed)
+	wg.Done()
+}
+
+func produce(channel chan Person) {
+	time.Sleep(3 * time.Second)
+	data := new(Person)
+	data.age = 10
+	data.name = "nghiatn"
+	fmt.Println("I am going to send this data: ", *data)
+	channel <- *data
+	data.name = "something else"
+	data.age = 100
+	fmt.Println("I have changed the data: ", *data)
+	wg.Done()
+}
