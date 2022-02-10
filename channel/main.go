1eb6023f379e00d9a3ad42a711f75d8d39768139
nghiatn <nghiatn@vng.com.vn>
2022-02-10 15:14:49 +0700

    + add maiin

--- channel/main.go
+++ channel/main.go
@@ -19,11 +19,28 @@ func main() {
 	go consume(channel)
 	go produce(channel)
 	wg.Wait()
+
+	// ch := make(chan int)
+	// wg.Add(2)
+	// go func() {
+	// 	i := <-ch
+	// 	fmt.Println(i)
+	// 	wg.Done()
+	// }()
+	// for j := 0; j < 5; j++ {
+	// 	wg.Add(2)
+	// 	go func() {
+	// 		ch <- 42
+	// 		wg.Done()
+	// 	}()
+	// }
+	// wg.Wait()
 }
 
 func consume(channel chan Person) {
 	start := time.Now()
 	data := <-channel
+	data = <-channel
 	elapsed := time.Since(start)
 	fmt.Printf("Hey, I have consumed this %v after waiting time %v\n", data, elapsed)
 	wg.Done()
